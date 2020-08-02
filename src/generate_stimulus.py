259ca3c568b5ff69305b8ded15b2044803aff1ab
Alon Weissfeld <alonweissfeld@Alons-MacBook-Pro.local>
2020-08-02 18:34:13 +0300

    Creat targets; Organize stimulus input structure.

--- src/generate_stimulus.py
+++ src/generate_stimulus.py
@@ -8,25 +8,53 @@ WAV_DIR = './stimuli'
 ATTENUATION_FACTOR = 0.5
 
 
-def generate(freq, duration):
+def generate(stimulus):
     """
     For a given frequency (cycles per second) and duration in seconds,
     generates a simple sine wave and saves it to disk.
     """
+    freq = stimulus.get('frequency')
+    duration = stimulus.get('duration')
+
     samples = np.arange(duration * SPS)
     interval = (2 * np.pi * freq) / SPS
 
     # Sample discrete sine values at each interval.
     waveform = np.sin(interval * samples)
 
+    # Apply decrease in volume targets in the waveform
+    targets = stimulus.get('targets')
+    create_targets(waveform, targets)
+
     # Convert to sign 16-bit integers and attenuate the loud signal
     waveform = np.int16(waveform * ATTENUATION_FACTOR * RANGE)
-    filepath = '{}/{}Hz_{}seconds.wav'.format(WAV_DIR, freq, duration)
+
+    length = len(targets.get('timings'))
+    filepath = '{}/{}Hz_{}s_{}targets.wav'.format(WAV_DIR, freq, duration, length)
     write(filepath, SPS, waveform)
 
-if __name__ == "__main__":
-    args = sys.argv
-    freq = float(args[1])
-    duration = float(args[2])
+def create_targets(waveform, targets):
+    """
+    Modifies the waveform to create targets (decrease in volume) by attenuating
+    it to perc * amplitude at the given targets timings (given in seconds).
+    """
+    timings = targets.get('timings')
+    duration = targets.get('duration')
+    percentage = targets.get('percentage')
 
-    generate(freq, duration)
+    for time in timings:
+        idx = time * SPS
+        waveform[idx:idx + int(SPS * duration)] *= percentage
+
+if __name__ == "__main__":
+    stimulus = {
+        'frequency': 500,
+        'duration': 10,
+        'targets': {
+            'timings': [1, 4, 7],
+            'duration': 0.5,
+            'percentage': 0.6
+        }
+    }
+
+    generate(stimulus)
